d94f315b451bc0390c9e828b6f4b8bc69a2f4f6e
aliners28 <62851070+aliners28@users.noreply.github.com>
2020-11-12 00:05:30 -0300

    primeiro programa git bash

---
+++ teste02.py
@@ -0,0 +1,11 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Dia = input('Dia:')
+Dia:
+>>> Mes = input('Mes:')
+Mes:
+>>> Ano =input('Ano: ')
+Ano: 
+>>> print('Voce nasceu no dia:' + ' de' + 'de' '.Correto?')
+Voce nasceu no dia: dede.Correto?
+>>> 
